c419f0410979f15927324cd529cd87f1946a906c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-01-18 15:15:48 +0500

    Создали первый проект на Rust

---
+++ chapter2/guessing_game/target/debug/build/libc-57c6cf1d8e02a330/build_script_build-57c6cf1d8e02a330.d
@@ -0,0 +1,5 @@
+/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/build/libc-57c6cf1d8e02a330/build_script_build-57c6cf1d8e02a330: /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.112/build.rs
+
+/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/build/libc-57c6cf1d8e02a330/build_script_build-57c6cf1d8e02a330.d: /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.112/build.rs
+
+/home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.112/build.rs:
---
+++ chapter2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/guessing_game: /home/rusl4n/projects/rust/chapter2/guessing_game/src/main.rs
