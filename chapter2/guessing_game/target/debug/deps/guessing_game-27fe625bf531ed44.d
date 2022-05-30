58d77cccb5516d5726ea431649d345fe2a4a4a97
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-30 09:32:44 +0500

    Репозиторий для небольших проектов на python3

--- chapter2/guessing_game/target/debug/deps/guessing_game-27fe625bf531ed44.d
+++
@@ -1,5 +0,0 @@
-/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/deps/guessing_game-27fe625bf531ed44: src/main.rs
-
-/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/deps/guessing_game-27fe625bf531ed44.d: src/main.rs
-
-src/main.rs:
--- chapter2/guessing_game/target/debug/deps/rand-6ebdcf552f503e6c.d
+++
@@ -1,9 +0,0 @@
-/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/deps/rand-6ebdcf552f503e6c.rmeta: /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/deps/librand-6ebdcf552f503e6c.rlib: /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/home/rusl4n/projects/rust/chapter2/guessing_game/target/debug/deps/rand-6ebdcf552f503e6c.d: /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/home/rusl4n/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
